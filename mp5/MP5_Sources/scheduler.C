3a6e0c76faae51499adb8674ec8a9f5714fb9089
Sneha Singh <snehasingh95@tamu.edu>
2022-11-04 16:27:56 -0500

    Scheduler constructed and added threads to ready queue

--- mp5/MP5_Sources/scheduler.C
+++ mp5/MP5_Sources/scheduler.C
@@ -45,23 +45,52 @@
 /* METHODS FOR CLASS   S c h e d u l e r  */
 /*--------------------------------------------------------------------------*/
 
+/*struct Node {
+	Thread* _thread;
+	struct Node* _next_node;
+};*/
+
+Node* Scheduler::ready_queue_front;
+Node* Scheduler::ready_queue_tail;
+int Scheduler::ready_thread_count;
+	
 Scheduler::Scheduler() {
-  assert(false);
-  Console::puts("Constructed Scheduler.\n");
+	//adding dummy node to the ready queue
+	Scheduler::ready_thread_count = 0;
+	Scheduler::ready_queue_front = Scheduler::ready_queue_tail = NULL;
+	Console::puts("Constructed Scheduler.\n");
 }
 
 void Scheduler::yield() {
-  assert(false);
+  	assert(false);
 }
 
 void Scheduler::resume(Thread * _thread) {
-  assert(false);
+  	assert(false);
 }
 
 void Scheduler::add(Thread * _thread) {
-  assert(false);
+	Node* new_ready_thread_node = new Node;
+	new_ready_thread_node -> _thread = _thread;
+	new_ready_thread_node -> _next_node = NULL;
+	
+	Machine::disable_interrupts();
+	
+	if(Scheduler::ready_queue_front==NULL){
+		//only dummy node in ready queue
+		Scheduler::ready_queue_front = new_ready_thread_node;
+		Scheduler::ready_queue_tail = new_ready_thread_node;
+	}else{
+		Scheduler::ready_queue_tail -> _next_node = new_ready_thread_node;
+		Scheduler::ready_queue_tail = new_ready_thread_node;
+	}
+	Scheduler::ready_thread_count++;
+	
+	Machine::enable_interrupts();
+	
+	Console::puts("Added Thread To Ready Queue.\n");
 }
 
 void Scheduler::terminate(Thread * _thread) {
-  assert(false);
+  	assert(false);
 }
